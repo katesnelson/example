c1dcd79b31578f3d836c94ca5de43c9b41895c9a
Katherine S Nelson <ksnelson@ksu.edu>
2022-02-10 10:14:24 -0600

    tidying BEA

--- test script.R
+++ test script.R
@@ -1,1 +1,20 @@
-fdfklansdklfnskd
+
+#Cleaning KS_BEA data and EDA
+
+library(tidyverse)
+
+bea <- read.csv("KS_BEA.csv")
+  glimpse(bea)
+  head(bea)
+  tail(bea)
+
+bea <- bea %>%
+  slice(1:3604)
+
+tail(bea)
+
+bea_tidy <- bea %>%
+  pivot_longer(cols = X2001:X2019,
+               names_to = "Year",
+               values_to = "Value")
+glimpse(bea_tidy)
